aa1d7c88b24c8307db084d5e1fdad11cfd9db63f
Henrique Guerra Messias <hgmessias@hotmail.com>
2024-03-28 21:44:05 -0300

    Atualização

--- scripts/script.js
+++ scripts/script.js
@@ -2,16 +2,65 @@ document.addEventListener('DOMContentLoaded', (event) => {
     event.preventDefault();
     alert("A página foi totalmente carregada e o js foi implantado")
 
-    loadUserData()
+    const form = document.querySelector('form')
+    form.addEventListener('submit', loadUserData);
+
+    // chamando a função para carregar o usuario
+    loadUserDataList();
+    //evento para chamar o alert
+    //alert("A pagina foi totalmente carregada ejs foi implantado")
+
+    //chamando a função para carregar os usuários
 })
 
 
 function loadUserData(event){
-    event.preventDefault()
+    event.preventDefault();
 
+    let listUser = []   //array - vetores
+
+    //capturando os valores e colocando eles dentro de um objeto (userData)
     const userData = {
+
+        //capturando os valores e colocando eles dentro das propriedades ou também atributos
+
         name: document.getElementById('name').value,
         email: document.getElementById('email').value,
         age: document.getElementById('age').value
     }
+
+    if(localStorage.getItem('users')) {
+        listUser = JSON.parse(localStorage.getItem('users'))
+    }
+
+    listUser.push(userData)
+    localStorage.setItem('users', JSON.stringify(listUser) )
+
+    console.log(listUser);
+    window.location.reload();
+
+}
+const loadUserDataList = () => {
+
+    const tableData = document.getElementById('tableBodyList');
+
+    if (localStorage.getItem('users')){
+       const listUsers = JSON.parse(localStorage.getItem('users'))  //json -> arquivo
+
+       let template = ""
+
+       listUsers.forEach(user => {
+            template += `
+                <tr>
+                    <td> ${user.name} </td>
+                    <td> ${user.email} </td>
+                    <td> ${user.age} </td>
+                </tr>
+            `
+       });
+       tableData.innerHTML = template
+    }else{
+        alert("Nenhum usuário cadastrado")
+    }
+
 }
